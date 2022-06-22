c5420963172b4d8b0f7e41e1d07cbf487c570bbf
Michal Moskal <michal@moskal.me>
2022-06-22 15:34:21 -0700

    specify CPUTAPID explicitly

--- flash.sh
+++ flash.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-openocd -c 'set FLASH_SIZE 0x20000' -f interface/stlink-v2.cfg -f target/stm32f1x.cfg -c 'program dist/blackmagic_all.bin verify reset exit 0x08000000'
+openocd \
+ -c 'set FLASH_SIZE 0x20000' \
+ -c 'set CPUTAPID 0x1ba01477' \
+ -f interface/stlink-v2.cfg -f target/stm32f1x.cfg \
+ -c 'program dist/blackmagic_all.bin verify reset exit 0x08000000' "$@"
